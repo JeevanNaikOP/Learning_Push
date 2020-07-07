a212b65e58bf5f44d89704ac70cd97ef8dd01f94
jeevan <naikjeevan@gmail.com>
2020-07-07 18:17:37 +0530

    learning to commit

---
+++ src/com/jee/person.kt
@@ -0,0 +1,7 @@
+package com.jee
+
+class person(var name: String) {
+    fun display(){
+        print("the name of the person is ${name}")
+    }
+}
---
+++ src/my first file.kt
@@ -0,0 +1,7 @@
+import com.jee.person
+
+fun main(args: Array<String>) {
+    var personObj= person("jeevan")
+    personObj.display()
+
+}
